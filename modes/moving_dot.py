198a0c085c72d65c766e7efc55e1141589436b58
Arne Schauf <arne.schauf@selfnet.de>
2014-01-07 23:47:36 +0100

    widemovingdot random colors

--- modes/moving_dot.py
+++ modes/moving_dot.py
@@ -24,6 +24,7 @@ class WideMovingDotMode(FixedColorMixin, BaseMode):
     last = 0
     color = (0, 0, 254)
     change = 1
+    fps = 50
 
     def __init__(self, width=5, *args, **kwargs):
         self.led_count = self.led_count + width * 2
@@ -36,9 +37,23 @@ class WideMovingDotMode(FixedColorMixin, BaseMode):
         if self.last >= self.led_count:
             self.change = -1
             self.last = self.led_count - self.width - 1
+            self.color = (0, 0, 0)
+            while self.color == (0, 0, 0):
+                self.color = (
+                    random.randint(0, 1) * 254,
+                    random.randint(0, 1) * 254,
+                    random.randint(0, 1) * 254
+                )
         elif self.last <= 0:
             self.change = 1
             self.last = self.width
+            self.color = (0, 0, 0)
+            while self.color == (0, 0, 0):
+                self.color = (
+                    random.randint(0, 1) * 254,
+                    random.randint(0, 1) * 254,
+                    random.randint(0, 1) * 254
+                )
         self.colors[self.last] = self.fixed_color if self.fixed_color else self.color
 
     def get_colors(self):
