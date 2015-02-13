2410255e846c5fbd756ed97868299e1674c89467
mpursley <mpursley@gmail.com>
2015-02-12 21:32:19 -0800

    Set it to flash black and white every second

--- flash_example.py
+++ flash_example.py
@@ -1,13 +1,19 @@
 from BlinkyTape import BlinkyTape
+import time
 
-bb = BlinkyTape('/dev/tty.usbmodemfa131')
+#bb = BlinkyTape('/dev/tty.usbmodemfa131')
+bb = BlinkyTape('COM8')
 
 while True:
 
     for x in range(60):
-        bb.sendPixel(10, 10, 10)
+        bb.sendPixel(100, 100, 100)
     bb.show()
+
+    time.sleep(.5)
 
     for x in range(60):
         bb.sendPixel(0, 0, 0)
     bb.show()
+
+    time.sleep(.5)
